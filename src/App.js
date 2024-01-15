820752c08a3e4363cd998b71dc548ced8dbec2bc
Alban LORILLARD <ext.alban.lorillard@sncf.fr>
2024-01-15 09:44:48 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { items } from './data';
+import {
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+  InfiniteLoader,
+  List,
+  WindowScroller,
+} from 'react-virtualized';
+function App() {
+
+  const [withScrollElement, setWithScrollElement] = useState(false);
+  const mainRef = useRef(null);
+  const infiniteLoaderRef = useRef(null);
+
+  const displayCount = 20;
+  const [displayedCount, setDisplayedCount] = useState(displayCount);
+
+  const cache = new CellMeasurerCache({
+    fixedWidth: true,
+    defaultHeight: 52,
+  });
+
+  const isRowLoaded = ({index}) => {
+    return index < displayedCount - 1;
+  };
+
+  const loadMoreRow = () => {
+    return new Promise((resolve) => {
+      setDisplayedCount(
+        displayedCount + displayCount < items.length ? displayedCount + displayCount : items.length,
+      );
+      resolve();
+    });
+  };
+
+  const rowRenderer = ({ key, index, style, parent, isScrolling }) => {
+    const item = items[index];
+    return (
+      <CellMeasurer key={key} cache={cache} parent={parent} columnIndex={0} rowIndex={index} style={style}>
+        <div key={key} style={style}>
+         Item numéro : {item}
+        </div>
+      </CellMeasurer>
+    );
+  };
+
+  return (
+    <div className="App">
+      <header>
+        This is a sticky header
+      </header>
+
+      <main ref={mainRef}>
+        This is the main part
+
+        withScrollElement: <input type="checkbox" checked={withScrollElement} onChange={() => setWithScrollElement(!withScrollElement)} />
+
+        <InfiniteLoader
+          isRowLoaded={isRowLoaded}
+          loadMoreRows={loadMoreRow}
+          rowCount={displayedCount}
+          ref={infiniteLoaderRef}
+        >
+          {({ onRowsRendered, registerChild }) => (
+            <WindowScroller
+             scrollElement={withScrollElement ? mainRef.current : window}
+            >
+              {({ height, isScrolling, scrollTop }) => (
+                <AutoSizer disableHeight>
+                  {({ width }) => (
+                    <List
+                      ref={registerChild}
+                      onRowsRendered={onRowsRendered}
+                      width={width}
+                      height={height}
+                      autoHeight
+                      overscanRowCount={20}
+                      rowCount={displayedCount}
+                      rowHeight={52}
+                      rowRenderer={rowRenderer}
+                      scrollTop={scrollTop}
+                      isScrolling={isScrolling}
+                    />
+                  )}
+                </AutoSizer>
+              )}
+            </WindowScroller>
+          )}
+
+        </InfiniteLoader>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
